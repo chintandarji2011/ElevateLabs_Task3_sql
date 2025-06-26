7618a100b2eafb5d53fa58bc1c87b51add277ca5
chintandarji2011 <chintandarji2011@gmail.com>
2025-06-26 14:01:43 +0530

    Add files via upload

---
+++ QueryTasks.sql
@@ -0,0 +1,40 @@
+/*------------ Step1. Use Select* and specific columns------------*/
+-- Retrive data from `department` table
+SELECT * FROM departments;
+-- Retrive data from `employees` table
+SELECT * FROM employees;
+
+-- retrive selected columns from both table with secific condition
+SELECT e.emp_id, e.emp_name,d.dep_name, e.salary, e.hire_date
+FROM employees e
+INNER JOIN departments d ON e.dep_id = d.dep_id;
+/*---------- Step2.Apply WHERE, AND, OR, LIKE, BETWEEN --------------*/
+-- WHERE + AND
+SELECT e.emp_name, d.dep_name, e.salary, e.hire_date
+FROM employees e
+INNER JOIN departments d ON e.dep_id = d.dep_id
+WHERE dep_name = 'IT' AND salary > 50000;
+-- WHERE + OR
+SELECT e.emp_name, d.dep_name, e.salary
+FROM employees e
+INNER JOIN departments d ON e.dep_id = d.dep_id
+WHERE dep_name = 'HR' OR salary > 50000;
+-- WHERE + LIKE
+SELECT e.emp_name, d.dep_name, e.salary
+FROM employees e
+INNER JOIN departments d ON e.dep_id = d.dep_id
+WHERE emp_name LIKE 'S%';
+-- WHERE + BETWEEN
+SELECT * FROM employees
+WHERE salary BETWEEN 40000 AND 70000;
+/* ----------------- Step3.Sort with ORDER BY -------------------*/
+-- Ascending
+SELECT * FROM employees
+ORDER BY salary;
+-- Descending
+SELECT * FROM employees
+ORDER BY salary DESC;
+-- LIMIT -- Top two highst salary
+SELECT * FROM employees
+ORDER BY salary DESC
+LIMIT 2;
---
+++ TableSchemas.sql
@@ -0,0 +1,37 @@
+-----Create the Database------------------
+--CREATE DATABASE elevatlabs_task3;
+
+-----------------------step1. Create Tables---------------------
+-----Create `departments` table------
+CREATE TABLE departments
+(
+  dep_id INT PRIMARY KEY,
+  dep_name VARCHAR(50) NOT NULL
+);
+----Create `employees` table---------
+CREATE TABLE employees
+(
+  emp_id INT PRIMARY KEY,
+  emp_name VARCHAR(50) NOT NULL,
+  dep_id INT,
+  salary decimal(10,2),
+  hire_date DATE
+);
+-------------------step.2 Insert data in tables--------------
+----Insert data in `department` table------
+INSERT INTO departments
+VALUES 
+(1,'HR'),(2,'Account'),(3,'IT');
+----Insert data in `employees` table-------
+INSERT INTO employees (emp_id, emp_name, dep_id, salary, hire_date)
+VALUES
+(1,'Rahesj Jadhav', 3, 60000, '2015-04-12'),
+(2,'Krunal Patil', 3, 45000, '2022-05-23'),
+(3,'Swami Rajnathan', 2, 34000, '2023-02-22'),
+(4,'Harsh Sharma', 1, 75000, '2018-04-11'),
+(5, 'Smruti Patel', 1, 85000, '2017=04-15');
+
+-----Add foreign key in employee table-----
+
+ALTER TABLE employees ADD CONSTRAINT FK_departments FOREIGN KEY(dep_id)
+REFERENCES departments(dep_id);
